44cb1e683e8a54148c17cc312129ba6a49dc049f
Liang <nj9106@gmail.com>
2023-11-28 14:13:36 +0800

    small changes

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
-
+export OPENAI_API_MODEL=gpt-4-1106-preview
 cd ./service
-nohup pnpm start > service.log &
+nohup pnpm start > service.log 2>&1 &
 echo "Start service complete!"
 
 
 cd ..
 echo "" > front.log
-nohup pnpm dev > front.log &
+nohup pnpm dev > front.log 2>&1 &
 echo "Start front complete!"
-tail -f front.log
